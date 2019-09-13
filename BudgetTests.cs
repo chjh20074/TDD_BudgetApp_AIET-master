d2642ff0bf06b491c7f48bbe3c9dd4b6071e23f9
chjh20074 <nutc1498b019@gmail.com>
2019-09-13 16:47:39 +0800

    新增失敗測試:輸入起始日在budget外

--- BudgetTests.cs
+++ BudgetTests.cs
@@ -52,6 +52,13 @@ namespace TDD_BudgetApp
             TotalAmountShouldBe(0, new DateTime(2019, 8, 31), new DateTime(2019, 8, 31));
         }
 
+        [Test]
+        public void period_no_overlapping_after_budget_lastDay()
+        {
+            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
+            TotalAmountShouldBe(0, new DateTime(2019, 10, 1), new DateTime(2019, 10, 1));
+        }
+
         //[Test]
         //public void period_inside_budget_month()
         //{
