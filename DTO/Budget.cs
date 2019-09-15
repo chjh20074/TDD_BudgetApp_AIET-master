8ec9f044609f9fe3c2c088f985879287eeb9ce77
chjh20074 <nutc1498b019@gmail.com>
2019-09-15 17:43:28 +0800

    將CreatePeriod移至Budget類別

--- DTO/Budget.cs
+++ DTO/Budget.cs
@@ -13,5 +13,10 @@ namespace TDD_BudgetApp.DTO
         public int DaysInMonth => DateTime.DaysInMonth(FirstDay.Year, FirstDay.Month);
 
         public DateTime LastDay => new DateTime(FirstDay.Year, FirstDay.Month, DaysInMonth);
+
+        public Period CreatePeriod()
+        {
+            return new Period(FirstDay, LastDay);
+        }
     }
 }
--- Service/Accounting.cs
+++ Service/Accounting.cs
@@ -25,14 +25,9 @@ namespace TDD_BudgetApp.Service
 
                 var dailyAmount = budget.Amount / budget.DaysInMonth;
 
-                return dailyAmount * period.OverlappingDays(CreatePeriod(budget));
+                return dailyAmount * period.OverlappingDays(budget.CreatePeriod());
             }
             return 0;
-        }
-
-        private static Period CreatePeriod(Budget budget)
-        {
-            return new Period(budget.FirstDay, budget.LastDay);
         }
     }
 }
