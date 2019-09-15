f23f95bd99b93552897f8b684e48e18ea6dda70b
chjh20074 <nutc1498b019@gmail.com>
2019-09-15 16:29:35 +0800

    通過測試:區間在budget firstday前

--- Service/Accounting.cs
+++ Service/Accounting.cs
@@ -8,17 +8,25 @@ namespace TDD_BudgetApp.Service
 {
     public class Accounting
     {
-        private readonly IBudgetRepos<Budget> _repos;
+        private readonly IBudgetRepos<Budget> _budgetRepos;
         
         public Accounting(IBudgetRepos<Budget> repos)
         {
-            _repos = repos;
+            _budgetRepos = repos;
         }
 
         public decimal TotalAmount(DateTime start, DateTime end)
         {
-            if (_repos.GetAll().Any())
+            var getAllBudgets = _budgetRepos.GetAll();
+            if (getAllBudgets.Any())
             {
+                var budget = getAllBudgets.First();
+                var firstDay = DateTime.ParseExact(budget.YearMonth + "01", "yyyyMMdd", null);
+                if (end < firstDay)
+                {
+                    return 0;
+                }
+
                 return new Period(start, end).Days();
             }
             return 0;
