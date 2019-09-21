de689708f42da3e6a58e9bb3fe441f35096cf949
chjh20074 <nutc1498b019@gmail.com>
2019-09-21 16:05:44 +0800

    新增失敗測試:期間在budget裡

--- BudgetTests.cs
+++ BudgetTests.cs
@@ -2,47 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using NSubstitute;
+using NSubstitute.Callbacks;
 using NUnit.Framework;
+using TDD_BudgetApp.DTO;
+using TDD_BudgetApp.Repos;
+using TDD_BudgetApp.Service;
 
 namespace TDD_BudgetApp
 {
     public class BudgetTests
     {
+        private Accounting _account;
+        private IRepos<Budget> _repos = Substitute.For<IRepos<Budget>>();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _account = new Accounting(_repos);
+        }
+
         [Test]
         public void no_budgets()
         {
-            Accounting account = new Accounting();
-            Assert.AreEqual(0, account.TotalAmount()); 
+            _repos.GetAll().Returns(new List<Budget>());
+            var start = new DateTime(2019, 8, 31);
+            var end = new DateTime(2019, 8, 31);
+            Assert.AreEqual(0, _account.TotalAmount(start, end)); 
         }
 
-        //[Test]
-        //public void period_inside_budget_month()
-        //{
-            
-        //}
+        [Test]
+        public void period_inside_budget_month()
+        {
+            _repos.GetAll().Returns(new List<Budget>
+            {
+                new Budget {YearMonth = "2019/9", Amount = 30}
+            });
+            var start = new DateTime(2019, 9, 1);
+            var end = new DateTime(2019, 9, 1);
+            Assert.AreEqual(1, _account.TotalAmount(start, end));
+        }
 
         //[Test]
         //public void period_no_overlapping_after_budget_lastDay()
         //{
-            
+
         //}
 
         //[Test]
         //public void period_no_overlapping_before_budget_firstDay()
         //{
-            
+
         //}
 
         //[Test]
         //public void period_overlapping_budget_firstDay()
         //{
-            
+
         //}
 
         //[Test]
         //public void period_overlapping_budget_lastDay()
         //{
-            
+
         //}
 
         //[Test]
@@ -62,13 +83,5 @@ namespace TDD_BudgetApp
         //{
 
         //}
-    }
-
-    public class Accounting
-    {
-        public decimal TotalAmount()
-        {
-            return 0;
-        }
     }
 }
---
+++ Service/Accounting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TDD_BudgetApp.DTO;
+using TDD_BudgetApp.Repos;
+
+namespace TDD_BudgetApp.Service
+{
+    public class Accounting
+    {
+        private readonly IRepos<Budget> _repos;
+
+        public Accounting(IRepos<Budget> repos)
+        {
+            _repos = repos;
+        }
+
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            var budgets = _repos.GetAll();
+            if (budgets.Count > 0)
+            {
+                return 0;
+            }
+
+
+            return 0;
+        }
+    }
+}
