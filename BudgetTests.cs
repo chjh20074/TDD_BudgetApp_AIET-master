ed01f6cd6844d7c4e57101cd3c062b1c9b9118df
chjh20074 <nutc1498b019@gmail.com>
2019-09-21 15:32:28 +0800

    新增失敗測試: 無bugets

--- BudgetTests.cs
+++ BudgetTests.cs
@@ -3,126 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
-using TDD_BudgetApp.DTO;
-using TDD_BudgetApp.Repos;
-using TDD_BudgetApp.Service;
 
 namespace TDD_BudgetApp
 {
     public class BudgetTests
     {
-        private Accounting _accounting;
-        private readonly IBudgetRepos<Budget> _budgetRepos = Substitute.For<IBudgetRepos<Budget>>();
-
-        [SetUp]
-        public void SetUp()
-        {
-            _accounting = new Accounting(_budgetRepos);
-        }
-
         [Test]
         public void no_budgets()
         {
-            GivenBudgets();
-            TotalAmountShouldBe(0, new DateTime(2019,9,1), new DateTime(2019, 9, 1));
+            Accounting account = new Accounting();
+            Assert.AreEqual(0, account.TotalAmount()); 
         }
 
-        [Test]
-        public void period_inside_budget_month()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30, });
-
-            TotalAmountShouldBe(1, new DateTime(2019, 9, 1), new DateTime(2019, 9, 1));
-        }
-
-        [Test]
-        public void period_no_overlapping_before_budget_firstDay()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
-            TotalAmountShouldBe(0, new DateTime(2019, 8, 31), new DateTime(2019, 8, 31));
-        }
-
-        [Test]
-        public void period_no_overlapping_after_budget_lastDay()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
-            TotalAmountShouldBe(0, new DateTime(2019, 10, 1), new DateTime(2019, 10, 1));
-        }
-
-        [Test]
-        public void period_overlapping_budget_firstDay()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
-            TotalAmountShouldBe(1, new DateTime(2019, 8, 31), new DateTime(2019, 9, 1));
-        }
-
-        [Test]
-        public void period_overlapping_budget_lastDay()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
-            TotalAmountShouldBe(1, new DateTime(2019, 9, 30), new DateTime(2019, 10, 1));
-        }
-
-        [Test]
-        public void invalid_period()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 30 });
-            TotalAmountShouldBe(0, new DateTime(2019, 9, 30), new DateTime(2019, 9, 1));
-        }
-
-        [Test]
-        public void Daily_Amount_is_10()
-        {
-            GivenBudgets(new Budget { YearMonth = "201909", Amount = 300 });
-            TotalAmountShouldBe(20, new DateTime(2019, 9, 1), new DateTime(2019, 9, 2));
-        }
-
-        [Test]
-        public void multiple_budgets()
-        {
-            GivenBudgets(
-                new Budget { YearMonth = "201909", Amount = 300 },
-                new Budget { YearMonth = "201910", Amount = 31 });
-            TotalAmountShouldBe(12, new DateTime(2019, 9, 30), new DateTime(2019, 10, 2));
-        }
-
-        private void GivenBudgets(params Budget[] budgets)
-        {
-            _budgetRepos.GetAll().Returns(budgets.ToList());
-        }
-
-        private void TotalAmountShouldBe(decimal expected, DateTime start, DateTime end)
-        {
-            Assert.AreEqual(expected, _accounting.TotalAmount(start, end));
-        }
         //[Test]
         //public void period_inside_budget_month()
         //{
-        //    GivenBudgets(new Budget { YearMonth = "201004", Amount = 30 });
-        //    TotalAmountShouldBe(1, new DateTime(2010, 4, 1), new DateTime(2010, 4, 1));
+            
         //}
 
-        //[Test(Description = "輸入的日期在有budget之前回傳0")]
-        //public void period_no_overlapping_before_budget_firstDay()
-        //{
-        //    GivenBudgets(new Budget { YearMonth = "201004", Amount = 30 });
-        //    TotalAmountShouldBe(0, new DateTime(2010, 3, 31), new DateTime(2010, 3, 31));
-        //}
-
-        //[Test(Description = "輸入的日期在有budget之後回傳0")]
+        //[Test]
         //public void period_no_overlapping_after_budget_lastDay()
         //{
-        //    GivenBudgets(new Budget { YearMonth = "201003", Amount = 31 });
-        //    GivenBudgets(new Budget { YearMonth = "201004", Amount = 30 });
-        //    TotalAmountShouldBe(0, new DateTime(2010, 5, 1), new DateTime(2010, 5, 1));
+            
+        //}
+
+        //[Test]
+        //public void period_no_overlapping_before_budget_firstDay()
+        //{
+            
+        //}
+
+        //[Test]
+        //public void period_overlapping_budget_firstDay()
+        //{
+            
+        //}
+
+        //[Test]
+        //public void period_overlapping_budget_lastDay()
+        //{
+            
         //}
 
         //[Test]
         //public void Daily_Amount_is_10()
         //{
-        //    GivenBudgets(new Budget { YearMonth = "201004", Amount = 300 });
-        //    TotalAmountShouldBe(20, new DateTime(2010, 4, 1), new DateTime(2010, 4, 2));
+
         //}
 
+        //[Test]
+        //public void invalid_period()
+        //{
+
+        //}
+
+        //[Test]
+        //public void multiple_budgets()
+        //{
+
+        //}
+    }
+
+    public class Accounting
+    {
+        public decimal TotalAmount()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repos/IBudgetRepos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace TDD_BudgetApp.Repos
-{
-    public interface IBudgetRepos<T>
-    {
-        List<T> GetAll();
-    }
-}
